351eddf727c2b9f57176db7f1011681e04d663d8
Damian <rosa.damian@gmail.com>
2017-10-12 16:38:14 +0200

    test

---
+++ wp-content/themes/dt-the7-child/functions.php
@@ -0,0 +1,26 @@
+<?php
+// load child theme textdomain
+/*
+function presscore_load_text_domain() {
+	load_child_theme_textdomain( 'presscore', get_stylesheet_directory() . '/languages' );
+}
+*/
+
+/**
+ * Your code here.
+ *
+ */
+ 
+ add_action( 'wp_enqueue_scripts', 'my_theme_enqueue_styles' );
+ function my_theme_enqueue_styles() {
+  
+	 $parent_style = 'parent-style'; // This is 'twentyfifteen-style' for the Twenty Fifteen theme.
+  
+	 wp_enqueue_style( $parent_style, get_template_directory_uri() . '/style.css' );
+	 wp_enqueue_style( 'child-style',
+		 get_stylesheet_directory_uri() . '/style.css',
+		 array( $parent_style ),
+		 wp_get_theme()->get('Version')
+	 );
+ }
+ ?>
